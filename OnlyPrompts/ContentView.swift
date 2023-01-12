25dc2c233473b63db1e65496b9d7e86b417be095
subcreation <in8media@gmail.com>
2023-01-11 19:23:40 -0800

    Added usageHistory to control selection of dashboard prompt

--- OnlyPrompts/ContentView.swift
+++ OnlyPrompts/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \ActiveDay.date, ascending: false)],
         animation: .default)
     var usageHistory: FetchedResults<ActiveDay>
+    @Environment(\.managedObjectContext) private var viewContext
     @ObservedObject var viewModel: ShuffleViewModel = ShuffleViewModel()
     
     @State var selection: Int? = 1
@@ -35,6 +36,10 @@ struct ContentView: View {
                             .opacity(0.0)
 #endif
                     }
+                    .listStyle(SidebarListStyle())
+                    .onAppear {
+                        updateActivity()
+                    }
                 }
             )
         } else {
@@ -56,5 +61,91 @@ struct ContentView: View {
     
     func reloadView() {
         self.viewModel.shuffle()
+    }
+    
+    func updateActivity() -> Void {
+        let today = Date()
+        if usageHistory.isEmpty {
+            addActiveDay(today)
+        } else {
+            let todayFound = isTodayAlreadyInHistory(today)
+            
+            if !todayFound {
+                addActiveDay(today)
+            } else {
+                mergeRaceConditionDuplicates()
+            }
+        }
+    }
+    
+    private func addActiveDay(_ today: Date) {
+        let newActiveDay = ActiveDay(context: viewContext)
+        newActiveDay.date = today
+        newActiveDay.minutesWriting = 0.0
+        newActiveDay.completedPoem = false
+        newActiveDay.requestedPrompt = true
+        
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    private func isTodayAlreadyInHistory(_ today: Date) -> Bool {
+        for i in 0..<usageHistory.count {
+            let activeDay = usageHistory[i]
+            if activeDay.date != nil {
+                if Calendar.current.isDate(activeDay.date!, inSameDayAs: today) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    private func mergeRaceConditionDuplicates() {
+        var previousActiveDay = usageHistory.first
+        if previousActiveDay != nil {
+            if previousActiveDay!.date != nil {
+                var activeDaysToRemove: [Int] = []
+                for i in 1..<usageHistory.count {
+                    let lastDay = previousActiveDay!
+                    let compareDay = usageHistory[i]
+                    if compareDay.date != nil {
+                        if Calendar.current.isDate(compareDay.date!, inSameDayAs: lastDay.date!) {
+                            if lastDay.completedPoem {
+                                compareDay.completedPoem = true
+                            }
+                            if lastDay.requestedPrompt {
+                                compareDay.requestedPrompt = true
+                            }
+                            if lastDay.minutesWriting > 0.0 {
+                                compareDay.minutesWriting += lastDay.minutesWriting
+                            }
+                            print("~-~ merged")
+                            activeDaysToRemove.append(i-1)
+                        }
+                    }
+                    previousActiveDay = compareDay
+                }
+                let indexSetForDayToRemove = IndexSet(activeDaysToRemove)
+                deleteActivity(offsets: indexSetForDayToRemove)
+            }
+        }
+    }
+    
+    private func deleteActivity(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { usageHistory[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
